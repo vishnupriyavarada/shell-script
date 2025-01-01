4a7c85f80bec9c32d570bc4b340b0d81f9da8674
vishnupriyavarada <vvarada09@gmail.com>
2025-01-01 17:05:12 +0530

    second file variable is added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 # This is comment
 
---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Krishna :: Hi Vishnu"
+echo "Vishnu :: Hello Krishna"
+echo "Krishna :: How are you doing ?"
+echo "Vishnu :: I am good . How about you ?"
