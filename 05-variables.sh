ef3a32f6a7ae9091841a818210aad4331d90c7c9
vishnupriyavarada <vvarada09@gmail.com>
2025-01-02 14:46:30 +0530

    05-variables.sh file is added

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+echo " Please enter your name :: " # text entered here will be added as value to variable
+
+read USERNAME # Here USERNAME is variable name
+
+echo "User name entered : " $USERNAME
