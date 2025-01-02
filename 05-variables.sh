cfd78c189b0de30a36bd7218f06f8306be04ae82
vishnupriyavarada <vvarada09@gmail.com>
2025-01-02 14:58:44 +0530

    -s added to read command

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,6 @@
 
 echo " Please enter your name :: " # text entered here will be added as value to variable
 
-read USERNAME # Here USERNAME is variable name
+read -s USERNAME # Here USERNAME is variable name
 
 echo "User name entered : " $USERNAME
