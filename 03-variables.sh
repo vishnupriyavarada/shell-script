99833d8e60b9705619ee64cc2d5a4a93bc795c6f
vishnupriyavarada <vvarada09@gmail.com>
2025-01-02 12:40:44 +0530

    03-variables.sh file added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+PERSON1=Krishna
+PERSON2=Vishnu
+
+echo "$PERSON1 :: Hi $PERSON2"
+echo "$PERSON2 :: Hello $PERSON1"
+echo "$PERSON1 :: How are you doing ?"
+echo "$PERSON2 :: I am good . How about you ?"
