c6350dc705c404dddbf177c665fee12ab121ba97
vishnupriyavarada <vvarada09@gmail.com>
2025-01-03 14:33:11 +0530

    Addition of two numbers

---
+++ 06-datatypes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Addition of two numbers. User must give number1 and number 2
+
+NUMBER1=$1
+NUMBER2=$2
+
+DATE=$(date)
+
+echo "script executed at ..$DATE"
+
+SUM=$($NUMBER1+$NUMBER2)
+
+echo "Addition of 2 numbers :: $SUM"
