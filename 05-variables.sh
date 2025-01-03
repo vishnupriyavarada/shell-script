6a43f5d68892bcf2a0acb44f40cf7720cf8d938b
vishnupriyavarada <vvarada09@gmail.com>
2025-01-03 12:22:21 +0530

    Password is added

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,12 @@
 
 echo " Please enter your name :: " # text entered here will be added as value to variable
 
-read -s USERNAME # Here USERNAME is variable name and when user enter's name, the value is assigned to USERNAME. 
-                 #-s is used to hide the data entered by user
+read USERNAME # Here USERNAME is variable name and when user enter's name, the value is assigned to USERNAME.                 
 
 echo "User name entered : " $USERNAME
+
+echo "Please enter your Password :: " 
+
+read -s PASSWORD  #-s is used to hide the data entered by user
+
+echo "Password entered is : " $PASSWORD
