094d4982d6bf2d5e77dde3fd345164cd11829bd9
vishnupriyavarada <vvarada09@gmail.com>
2025-01-03 12:13:51 +0530

    changes in 05-variables.sh

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,7 @@
 
 echo " Please enter your name :: " # text entered here will be added as value to variable
 
-read -s USERNAME # Here USERNAME is variable name
+read -s USERNAME # Here USERNAME is variable name and when user enter's name, the value is assigned to USERNAME. 
+                 #-s is used to hide the data entered by user
 
 echo "User name entered : " $USERNAME
