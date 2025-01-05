52b728da72d50520a2006dcdfd32066818e5a303
vishnupriyavarada <vvarada09@gmail.com>
2025-01-05 12:20:39 +0530

    conditions file added

---
+++ 10-install-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#	1. Check if the user running the script is root user or not
+#	if root user 
+#		allow him
+#	else
+#		show error properly and exit
+          
+#	run install command
+	
+#	2. check installation is success or not
+	
+#	if success
+#		 our task is done
+#	else
+#throw the error message
+
+#-------------------------------------------------------------
+
+# Check if the user running the script is root user or not
+
+ USERID=$(id -u)
+
+ if [ $USERID -ne 0]
+ then
+    echo "ERROR: You must have sudo/root user access to execute this scriptr"
+ fi
+
+ dnf install mysql -y
