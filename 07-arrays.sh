563acd0532b1ae6276adb5532a130619eb0c722a
vishnupriyavarada <vvarada09@gmail.com>
2025-01-05 06:41:29 +0530

    Arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 MOVIES=("luckybhaskar","amaran","puspa2") # index starts from 0 and size of this movies array is 3
+                                          # Here instead of one value you are storing multiple values. This is array
 
 echo "First Movie : ${MOVIES[0]}"
+echo "Second Movie : ${MOVIES[1]}"
+echo "Third Movie : ${MOVIES[2]}"
