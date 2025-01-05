cf4475b2fc4daa7791dfde53022b5a0141552539
vishnupriyavarada <vvarada09@gmail.com>
2025-01-05 11:38:59 +0530

    conditions file added

---
+++ 09-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Print a number greater than 100 or not
+
+# get the number from user
+# check if the number is greater than 100 or not
+# if greater than 100 than print number is greater than 100
+# else print number is less than 100
+
+# Get number from user
+read NUMBER
+
+# check if the number is greater than 100 or not
+# -gt, -lt, -eq, -ge, -le 
+if [ $NUMBER -gt 100 ]
+then
+    echo "Number ${NUMBER} is greater than 100"
+else
+    echo "Number ${NUMBER} is less than 100"
+fi
