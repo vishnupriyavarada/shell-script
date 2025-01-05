5f539b1b19af7acc6864c4007d83fa733b62b8f0
vishnupriyavarada <vvarada09@gmail.com>
2025-01-05 06:46:24 +0530

    Array file modified - removed commas in between the values

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVIES=("luckybhaskar","amaran","puspa2") # index starts from 0 and size of this movies array is 3
+MOVIES=("luckybhaskar" "amaran" "puspa2") # index starts from 0 and size of this movies array is 3
                                           # Here instead of one value you are storing multiple values. This is array
 
 echo "First Movie : ${MOVIES[0]}"
