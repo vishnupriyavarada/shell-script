94d4d4f46c581e2972dd658accb3b35467c65ecd
vishnupriyavarada <vvarada09@gmail.com>
2025-06-18 20:50:44 +0530

    07-array.sh is modified

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,5 +7,5 @@
 	echo "Home directory of the current user who is executing the script: $HOME"
 	echo "Which user is running the script: $USER"
 	echo "Process id of current script: $$"	
-    sleep 60 &
+    sleep 60 & # sleep 60 - fore ground . sleep 60 & - back ground
     echo "Process id of last command in the background: $!"
