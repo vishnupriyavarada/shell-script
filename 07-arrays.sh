952c94e1bb570a404f9f0d6595e0e862923dce0e
vishnupriyavarada <vvarada09@gmail.com>
2025-06-18 19:00:26 +0530

    07-array.sh is modified

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,5 +6,10 @@ MOVIES=("luckybhaskar" "amaran" "puspa2") # index starts from 0 and size of this
 echo "First Movie : ${MOVIES[0]}"
 echo "Second Movie : ${MOVIES[1]}"
 echo "Third Movie : ${MOVIES[2]}"
-
 echo "All movies in the variable MOVIES is : ${MOVIES[@]}"
+
+echo "First Movie without {}: $MOVIES[0]"
+echo "Second Movie without {}: $MOVIES[1]"
+echo "Third Movie without {}: $MOVIES[2]"
+
+echo "All movies in the variable MOVIES without {} is : $MOVIES[@]"
