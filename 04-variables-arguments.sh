de662b3c72b28615a29f1ef1ee6c58d0ab5b99c7
vishnupriyavarada <vvarada09@gmail.com>
2025-06-18 14:55:30 +0530

    04-variables-arguments.sh file added

--- 04-variables-arguments.sh
+++ 04-variables-arguments.sh
@@ -1,7 +1,8 @@
 #!/bin/bash/
 
-PERSON1=$1
-PERSON2=$2
+# Variable values passed from outside the script as Arguments $1 - argument1 $2- argument2
+PERSON1=$1 # argument 1
+PERSON2=$2 # argument 2
 
 echo "$PERSON1 :: Hi $PERSON2"
 echo "$PERSON2 :: Hello $PERSON1"
